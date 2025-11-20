b554200afe35837b7957e09885c2df6d12ef743b
LinaMarinj <162225461+LinaMarinj@users.noreply.github.com>
2025-11-20 17:22:57 -0500

    Advance studentRetentionCard

--- client/src/components/studentRetentionManagement/StudentRetentionService.jsx
+++ client/src/components/studentRetentionManagement/StudentRetentionService.jsx
@@ -5,6 +5,24 @@ import { motion, useInView } from "framer-motion";
 function StudentRetentionService() {
   const { t } = useTranslation();
 
+  const expandableTransition = {
+    initial: { opacity: 0, scale: 0.5 },
+    whileInView: { opacity: 1, scale: 1 },
+    transition: { duration: 0.8, ease: "easeOut" },
+    viewport: { once: true },
+  };
+
+  const floatSnake = (delay = 0) => ({
+    initial: { y: -10 },
+    animate: { y: [0, -25, 0] },
+    transition: {
+      duration: 3,
+      repeat: Infinity,
+      ease: "linear",
+      delay,
+    },
+  });
+
   return (
     <>
       <section className="text-center mt-10 md:mt-0">
@@ -28,9 +46,12 @@ function StudentRetentionService() {
         </p>
       </section>
 
-      <section className="grid grid-cols-1 md:grid-cols-3 grid-rows-1 gap-8 mt-10 place-items-center">
-        <div className="flex flex-col items-center border-4 p-5 border-[#FFBA08] rounded-xl bg-[#FFF8E6] h-[420px] w-[350px]">
-          <span className="text-2xl block text-left font-impact m-5 text-[#FFBA08]">
+      <section className="grid grid-cols-1 md:grid-cols-3 grid-rows-1 gap-8 mt-16 place-items-center">
+        <div className="relative flex flex-col items-center border-4 p-5 border-[#FFBA08] rounded-xl bg-[#FFF8E6] h-[420px] w-[350px]">
+          <div className="absolute -top-6 -left-5 ml-2 bg-[#FFBA08] p-3 rounded-xl shadow-lg flex items-center justify-center">
+            <span className="text-3xl">📚</span>
+          </div>
+          <span className="text-3xl block text-left font-impact m-5 text-[#FFBA08]">
             {t("studentRetentionManagement.services.academic.title")}
           </span>
           <p className="text-base mb-3 text-blueBase text-justify">
@@ -57,8 +78,7 @@ function StudentRetentionService() {
 
         <div className="hidden md:block row-span-2 rounded-full flex-col items-center justify-center border-[6px] p-5 border-[#FFBA08] h-[350px] w-[350px] border-5">
           <span className="flex justify-center text-6xl align-middle ">🎓</span>
-
-          <p className="text-3xl text-[#222D56] font-impact text-center p-5">
+          <p className="text-4xl text-[#222D56] font-impact text-center p-5">
             {t(
               "studentRetentionManagement.studentRetentionService.centralTitle"
             )}{" "}
@@ -76,8 +96,11 @@ function StudentRetentionService() {
           </p>
         </div>
 
-        <div className="flex flex-col items-center border-4 p-5 border-[#2C9BC7] rounded-xl bg-[#CCEDE8] h-[420px] w-[350px]">
-          <span className="text-2xl block text-left font-impact m-5 text-[#2C9BC7]">
+        <div className="relative flex flex-col items-center border-4 p-5 border-[#2C9BC7] rounded-xl bg-[#CCEDE8] h-[420px] w-[350px]">
+          <div className="absolute -top-6 -right-5 ml-2 bg-[#2C9BC7] p-3 rounded-xl shadow-lg flex items-center justify-center">
+            <span className="text-3xl">💙</span>
+          </div>
+          <span className="text-3xl block text-left font-impact m-5 text-[#2C9BC7]">
             {t("studentRetentionManagement.services.socioEmotional.title")}
           </span>
           <p className="text-base mb-3 text-blueBase text-justify">
@@ -106,8 +129,11 @@ function StudentRetentionService() {
           </ul>
         </div>
 
-        <div className="flex flex-col items-center border-4 p-5 border-[#7C76B5] rounded-xl bg-[#D3C3E3] h-[420px] w-[350px]">
-          <span className="text-2xl block text-left font-impact m-5 text-[#7C76B5]">
+        <div className="relative flex flex-col items-center border-4 p-5 border-[#7C76B5] rounded-xl bg-[#D3C3E3] h-[420px] w-[350px]">
+          <div className="absolute  -bottom-6 -left-5 ml-2 bg-[#7C76B5] p-3 rounded-xl shadow-lg flex items-center justify-center">
+            <span className="text-3xl">💰</span>
+          </div>
+          <span className="text-3xl block text-left font-impact m-5 text-[#7C76B5]">
             {t("studentRetentionManagement.services.socioEconomic.title")}
           </span>
           <p className="text-base mb-3 text-blueBase text-justify">
@@ -129,8 +155,11 @@ function StudentRetentionService() {
           </ul>
         </div>
 
-        <div className="flex flex-col items-center border-4 p-5 border-[#0B8F84] rounded-xl bg-[#CCEDE8] h-[420px] w-[350px]">
-          <span className="text-2xl block text-left font-impact m-5 text-[#0B8F84]">
+        <div className="relative flex flex-col items-center border-4 p-5 border-[#0B8F84] rounded-xl bg-[#CCEDE8] h-[420px] w-[350px]">
+          <div className="absolute -bottom-6 -right-5 ml-2 bg-[#0B8F84] p-3 rounded-xl shadow-lg flex items-center justify-center">
+            <span className="text-3xl">🎯</span>
+          </div>
+          <span className="text-3xl block text-left font-impact m-5 text-[#0B8F84]">
             {t("studentRetentionManagement.services.socioVocational.title")}
           </span>
           <p className="text-base mb-3 text-blueBase text-justify">
@@ -154,38 +183,55 @@ function StudentRetentionService() {
         </div>
       </section>
 
-      <section className="flex justify-center mt-5 items-center min-h-screen">
-        <div className="relative p-[5px] m-5 rounded-2xl bg-gradient-to-r from-[#222D56] via-[#0B8F84] to-[#FFBA08] w-[1000px] h-[450px] shadow-2xl">
-          <section className="bg-white text-center p-10 w-full h-full rounded-[14px] flex flex-col justify-center">
-            <div className="mb-5 mt-5">
-              <span className="text-5xl align-middle">🎯</span>
-              <span className="text-5xl align-middle">💙</span>
-              <span className="text-5xl align-middle">💰</span>
-              <span className="text-5xl align-middle">📚</span>
-            </div>
-            <div>
-              <h5 className="text-3xl md:text-5xl m-5 md:mt-10 font-impact text-[#FFBA08]">
-                TU PERMANENCIA{" "}
-                <span className="text-[#222D56]">ES NUESTRAPRIORIDAD</span>
+      <motion.div {...expandableTransition}>
+        <section className="flex justify-center items-center min-h-screen mt-10 md:mt-10 mb-2 px-3">
+          <div className="relative p-[5px] rounded-3xl bg-gradient-to-r from-[#222D56] via-[#0B8F84] to-[#FFBA08] w-full max-w-[1020px] h-auto shadow-[0_30px_70px_rgba(0,0,0,0.3)]">
+            <section className="bg-white text-center p-8 md:p-10 w-full h-full rounded-[14px] flex flex-col justify-center">
+              <div className="flex justify-center text-4xl md:text-6xl mt-3 md:mt-4">
+                <motion.span {...floatSnake(0)} className="mx-2">
+                  🎯
+                </motion.span>
+                <motion.span {...floatSnake(0.3)} className="mx-2">
+                  💙
+                </motion.span>
+                <motion.span {...floatSnake(0.6)} className="mx-2">
+                  💰
+                </motion.span>
+                <motion.span {...floatSnake(0.9)} className="mx-2">
+                  📚
+                </motion.span>
+              </div>
+              <h5 className="text-4xl md:text-5xl mt-6 font-impact text-[#FFBA08] px-3 leading-tight">
+                {t("studentRetentionManagement.studentRetentionCard.title")}
+                <span className="text-[#222D56]">
+                  {" "}
+                  {t("studentRetentionManagement.studentRetentionCard.span")}
+                </span>
               </h5>
-              <p className="text-blueBase text-lg max-w-2xl mx-auto">
-                Un ecosistema completo diseñado para acompañarte en cada paso de
-                tu camino universitario.{" "}
+              <p className="text-blueBase text-base md:text-xl mt-4 px-3">
+                {t(
+                  "studentRetentionManagement.studentRetentionCard.description"
+                )}{" "}
                 <strong className="text-[#28A499]">
-                  Juntos construimos tu éxito
+                  <br />{" "}
+                  {t("studentRetentionManagement.studentRetentionCard.strong")}{" "}
                 </strong>
-                .
               </p>
-            </div>
-            <div>
-              {" "}
-              <button className="px-8 py-3 mt-8 text-white text-xl font-impact border-2 rounded-3xl transition-all duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg  bg-gradient-to-r from-[#FFBA08] to-[#0B8F84]">
-                ¡COMIENZA AHORA!
-              </button>
-            </div>
-          </section>
-        </div>
-      </section>
+              <div className="flex justify-center">
+                <button className="group relative overflow-hidden w-full max-w-[310px] h-[60px] md:h-[75px] mt-8 text-white text-xl md:text-2xl font-impact rounded-full shadow-xl bg-gradient-to-r from-[#FFBA08] to-[#0B8F84]  transform hover:scale-105 transition-all duration-500">
+                  <span className="relative">
+                    {" "}
+                    {t(
+                      "studentRetentionManagement.studentRetentionCard.btnStartNow"
+                    )}
+                  </span>
+                  <div class="absolute inset-0 bg-[#ffffff3a] transform scale-x-0 group-hover:scale-x-100 transition-transform origin-left duration-500"></div>
+                </button>
+              </div>
+            </section>
+          </div>
+        </section>
+      </motion.div>
     </>
   );
 }
