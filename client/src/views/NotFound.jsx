0dd8df9a31e653660d6c0e3af1802bcca6aad3c2
LinaMarinj <162225461+LinaMarinj@users.noreply.github.com>
2025-12-01 23:23:27 -0500

    Add corporate colors to tailwind.config

--- client/src/views/NotFound.jsx
+++ client/src/views/NotFound.jsx
@@ -10,10 +10,10 @@ function NotFound() {
       className="grid justify-center items-center h-screen bg-cover text-center"
     >
       <div>
-        <h1 id="errorNumber" className="font-bold block m-auto">
+        <h1 id="errorNumber" className="font-bold block m-auto text-dark-blue">
           404
         </h1>
-        <p className="text-5xl font-impact text-center text-darkBlue">
+        <p className="text-5xl font-impact text-center text-dark-blue">
           {t("notFound.title")}
         </p>
         <p className="mt-1 text-2xl text-gray-500 font-medium text-center">
@@ -23,7 +23,7 @@ function NotFound() {
         <Link to="/">
           <button
             id="volver-inicio"
-            className="group relative overflow-hidden w-full max-w-[200px] text-xl mt-5 p-3 border-2 rounded-3xl md:text-2xl text-darkBlue font-bold shadow-2xl shadow-[#222D56] transform hover:scale-105 transition-all duration-500"
+            className="group relative overflow-hidden w-full max-w-[200px] text-lg md:text-xl mt-5 p-3 border-2 rounded-3xl text-dark-blue font-bold shadow-2xl shadow-[#222D56] transform hover:scale-105 transition-all duration-500"
           >
             {t("notFound.button")}
             <div class="absolute inset-0 bg-[#222d562f] transform scale-x-0 group-hover:scale-x-100 transition-transform origin-left duration-500"></div>
