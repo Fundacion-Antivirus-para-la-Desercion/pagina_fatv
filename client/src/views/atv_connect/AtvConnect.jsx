18d75f35dee91d2e869822f865c34f8324af21df
LinaMarinj <162225461+LinaMarinj@users.noreply.github.com>
2026-02-02 10:03:47 -0500

    add component SchoolSubjectAtvConnect

--- client/src/views/atv_connect/AtvConnect.jsx
+++ client/src/views/atv_connect/AtvConnect.jsx
@@ -1,45 +1,24 @@
-import { useEffect, useMemo, useState } from "react";
 import BannerAtvConnectEn from "../../assets/images/views/atvConnect/academic-tutoring.webp";
 import BannerAtvConnectEs from "../../assets/images/views/atvConnect/tutorias-academicas.webp";
 import LogoAtvConecta from "../../assets/images/views/proVocacion/information/logo-atv-conecta-blanco.webp";
 import BannerView from "../../components/Banner-views/BannerView";
-import Javi from "../../assets/images/views/javi/Javi-4.png";
 import Graduation from "../../assets/images/views/atvConnect/graduados.png";
 import Birrete from "../../assets/images/views/atvConnect/birrete.webp";
 import { LuGraduationCap } from "react-icons/lu";
 import { LuBookOpen } from "react-icons/lu";
-import { CiHeart } from "react-icons/ci";
 import { HiBadgeCheck } from "react-icons/hi";
 import { FaRegHeart } from "react-icons/fa";
-
 
 import { motion } from "framer-motion";
 import CounterNumeric from "../../components/ContextData/CounterNumer.jsx";
 import { useTranslation } from "react-i18next";
 import TestimonialsAtvConnect from "../../views/atv_connect/testimonialsAtvConnect/TestimonialsAtvConnect.jsx";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 import BtnAtv from "./btnAtvConnect/BtnAtvConnect.jsx";
 import ServicesAtvConnect from "./servicesAtvConnect/ServicesAtvConnect.jsx";
+import SchoolSubjectsAtvConnect from "./schoolSubjects/SchoolSubjectsAtvConnect.jsx";
 
 function AtvConnect() {
-  const [init, setInit] = useState(false);
-
   const { t } = useTranslation();
-
-  useEffect(() => {
-    initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
-      await loadSlim(engine);
-      //await loadBasic(engine);
-    }).then(() => {
-      setInit(true);
-    });
-  }, []);
 
   const floatSnake = (delay = 0) => ({
     initial: { y: -10 },
@@ -66,112 +45,12 @@ function AtvConnect() {
     viewport: { once: true, amount: 0.6 },
   };
 
-  const particlesLoaded = (container) => {
-    console.log("particles conatiner");
-    console.log(container);
-  };
-
-  const options = useMemo(
-    () => ({
-      fullScreen: {
-        enable: false, // <--- ESTO ES LO MÁS IMPORTANTE
-        zIndex: 0, // Asegura un z-index bajo si es necesario
-      },
-      fpsLimit: 120,
-      particles: {
-        number: {
-          value: 200,
-          density: {
-            enable: true,
-          },
-        },
-        color: {
-          value: "#ffffff",
-        },
-        shape: {
-          type: "circle",
-        },
-        opacity: {
-          value: 0.7,
-          random: true,
-        },
-        size: {
-          value: { min: 1, max: 3 },
-          random: true,
-        },
-        move: {
-          enable: true,
-          speed: 2,
-          direction: "bottom",
-          random: true,
-          straight: false,
-          outModes: {
-            default: "out",
-          },
-        },
-      },
-      detectRetina: true,
-    }),
-    [],
-  );
-
   const expandableTransition = (delayValue) => ({
     initial: { opacity: 0, scale: 0.5 },
     whileInView: { opacity: 1, scale: 1 },
     transition: { duration: 0.8, ease: "easeOut", delay: delayValue },
     viewport: { once: true },
   });
-
-  const subjects = [
-    {
-      subject: t("atvConnect.subjects.subject_list.calculus"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.physics"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.chemistry"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.programming"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.statistics"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.algebra"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.accounting"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.economics"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.english"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.biology"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.mathematics"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.data_bases"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.algorithms"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.thermodynamics"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.circuits"),
-    },
-    {
-      subject: t("atvConnect.subjects.subject_list.finance"),
-    },
-  ];
 
   return (
     <>
@@ -357,43 +236,7 @@ function AtvConnect() {
         </div>
       </section>
 
-      <section className="grid grid-cols-1 md:grid-cols-[30%,70%] bg-dark-blue p-5 md:p-10 md:pt-20 md:pb-[184px] relative mb-10">
-        <Particles
-          id="tsparticles"
-          particlesLoaded={particlesLoaded}
-          options={options}
-          className="absolute top-0 left-0 w-full h-full z-0 bg-dark-blue"
-        />
-        <div className="m-10 md:m-2 relative text-center md:text-left">
-          <span className="text-lg text-primary-purple font-impact">
-            {t("atvConnect.subjects.span")}{" "}
-          </span>
-          <h6 className="text-4xl md:text-5xl text-white font-impact">
-            {t("atvConnect.subjects.title")}
-          </h6>
-        </div>
-
-        <div className="flex flex-wrap items-center relative">
-          <section className="flex flex-wrap justify-center items-center text-white p-3 gap-4">
-            {subjects.map((subjects, index) => (
-              <span
-                key={index}
-                className="rounded-3xl border border-white border-opacity-35 text-base py-2 px-5 cursor-pointer"
-              >
-                {subjects.subject}
-              </span>
-            ))}
-          </section>
-        </div>
-
-        <div className="relative">
-          <img
-            className="block mx-auto md:absolute md:top-[-90px] max-w-[190px] md:max-w-[200px]"
-            src={Javi}
-            alt=""
-          />
-        </div>
-      </section>
+      <SchoolSubjectsAtvConnect />
 
       <TestimonialsAtvConnect />
     </>
