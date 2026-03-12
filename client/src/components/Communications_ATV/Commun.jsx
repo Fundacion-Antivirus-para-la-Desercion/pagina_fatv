0ca757ac7d8adf5947b5a94d7f392b10692f3dd3
LinaMarinj <linamarin2011@gmail.com>
2026-03-12 16:41:51 -0500

    ajust commun

--- client/src/components/Communications_ATV/Commun.jsx
+++ client/src/components/Communications_ATV/Commun.jsx
@@ -1,49 +1,50 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import "./Commun.css";
 import CommunicationsTeam from "../../../public/img_Communications/communicationsTeam.webp";
 
 function Commun() {
   const { t } = useTranslation();
 
   return (
-    <div id="communications flex flex-col items-center">
-      <div className="data_analytics">
-        <div className="data_analytics--cnt">
-          <div className="data_analytics--content">
-            <div className="data_analytics--text">
-              <p className="text-sm tracking-nm uppercase text-left text-blue-base">
-                {t("communications_ATV.commun.our_model")}
-              </p>
-              <h1 className="lineSubtitle text-xl text-blue-base font-impact">
-                {t("communications_ATV.commun.subtitle")}
-              </h1>
-
-              <p className="text-justify text-blue-base">
-                {t("communications_ATV.commun.paragraph_1")}
-                <br />
-                <br />
-                {t("communications_ATV.commun.paragraph_2")}
-                <br />
-                <br />
-                {t("communications_ATV.commun.paragraph_3")}
-                <br />
-                <br />
-                {t("communications_ATV.commun.paragraph_4")}
-              </p>
-            </div>
-            <div className="data_analytics--image">
-              <picture>
-                <img
-                  src={CommunicationsTeam}
-                  alt={t("communications_ATV.commun.alt_image")}
-                />
-              </picture>
-            </div>
+    <>
+      <section
+        id="communications"
+        className="container mx-auto flex flex-col items-center gap-12 px-8 py-16 lg:flex-row lg:gap-24"
+      >
+        <div className="w-full lg:w-2/5">
+          <div>
+            <p className="text-sm tracking-nm uppercase text-left text-blue-base">
+              {t("communications_ATV.commun.our_model")}
+            </p>
+            <h1 className="lineSubtitle text-4xl md:text-5xl mb-4 block w-full leading-[1.08] tracking-[-1px] text-blue-base font-impact sm:tracking-[-2px]">
+              {t("communications_ATV.commun.subtitle")}
+            </h1>
+          </div>
+          <div>
+            <p className="text-justify text-base md:text-lg leading-relaxed text-blue-base">
+              {t("communications_ATV.commun.paragraph_1")}
+              <br />
+              <br />
+              {t("communications_ATV.commun.paragraph_2")}
+              <br />
+              <br />
+              {t("communications_ATV.commun.paragraph_3")}
+              <br />
+              <br />
+              {t("communications_ATV.commun.paragraph_4")}
+            </p>
           </div>
         </div>
-      </div>
-    </div>
+
+        <div className="w-full lg:w-1/2">
+          <img
+            src={CommunicationsTeam}
+            alt={t("communications_ATV.commun.subtitle")}
+            className="mx-auto w-full max-w-[700px] rounded-3xl border-[4px] border-solid border-white shadow-2xl"
+          />
+        </div>
+      </section>
+    </>
   );
 }
 
