6ed34af98cc54a3ad125268fd1b95cd0e455b16b
LinaMarinj <linamarin2011@gmail.com>
2026-03-17 14:36:08 -0500

    Enhance layout animation by wrapping Outlet in motion section; add slideFromTopBody animation for improved page transition effects

--- client/src/components/motion/constants/Animations.js
+++ client/src/components/motion/constants/Animations.js
@@ -45,3 +45,28 @@ export const slideFromTop = ({
   transition: { duration: 0.8, ease: "easeOut", delay: 0.3, ...transition },
   viewport: { once: true, amount: 0.6, ...viewport },
 });
+
+
+/**
+ * Animación para deslizar desde arriba con opacidad hacia abajo, diseñada para el cuerpo principal de la página. Se puede personalizar con parámetros opcionales.
+ * @param {Object} param0 - Configuración de la animación (opcional).
+ * @param {Object} param0.initial - Estado inicial de la animación (opcional).
+ * @param {Object} param0.animate - Estado de animación final (opcional).
+ * @param {Object} param0.transition - Configuración de la transición (opcional).
+ * @param {Object} param0.viewport - Configuración del viewport (opcional).
+ * @returns {Object} Configuración de la animación para Framer Motion.
+ */
+export const slideFromTopBody = ({
+  initial,
+  animate,
+  transition,
+} = {}) => ({
+  initial: { y: -75, opacity: 0, ...initial },
+  animate: { y: 0, opacity: 1, ...animate },
+  transition: { 
+    duration: 0.4, 
+    ease: "easeOut",
+    opacity: { duration: 0.01 },
+    ...transition,
+  },
+});
