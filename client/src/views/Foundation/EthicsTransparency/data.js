86eb24783e08fb60792b36b3854d9f4819205cd2
LinaMarinj <linamarin2011@gmail.com>
2026-06-09 13:10:36 -0500

    Refactor Ethics Transparency section: add PDF documents and update structure

---
+++ client/src/views/Foundation/EthicsTransparency/data.js
@@ -0,0 +1,53 @@
+export const pdfTabs = [
+  { id: "all", labelKey: "ethicsTransparency.pdf.tabs.all" },
+  { id: "tributario", labelKey: "ethicsTransparency.pdf.tabs.tributario" },
+  { id: "financiero", labelKey: "ethicsTransparency.pdf.tabs.financiero" },
+  { id: "donaciones", labelKey: "ethicsTransparency.pdf.tabs.donaciones" },
+  { id: "reporte", labelKey: "ethicsTransparency.pdf.tabs.reporte" },
+  { id: "ptee", labelKey: "ethicsTransparency.pdf.tabs.ptee" },
+
+];
+
+export const pdfDocuments = [
+  {
+    id: "tributario-2025",
+    titleKey: "ethicsTransparency.pdf.docs.tributario2025.title",
+    category: "tributario",
+    categoryLabelKey: "ethicsTransparency.pdf.tabs.tributario",
+    year: "2025",
+    file: "https://fundacionantivirus-my.sharepoint.com/:b:/g/personal/desarrolladora_fundacionantivirusparaladesercion_org/IQAV_NFLqhYNQI_t6t76OpD3AXB9H8c1GzS65Pi6R0sZUtY?e=IYq5BY&download=1",
+  },
+  {
+    id: "financiero-2025",
+    titleKey: "ethicsTransparency.pdf.docs.financiero2025.title",
+    category: "financiero",
+    categoryLabelKey: "ethicsTransparency.pdf.tabs.financiero",
+    year: "2025",
+    file: "https://fundacionantivirus-my.sharepoint.com/:b:/g/personal/desarrolladora_fundacionantivirusparaladesercion_org/IQB8F8F6vSPASqTXnUr5iu0bAUj2_moTXE_UkRhrb5zxY_8?e=jKAEKo&download=1",
+  },
+  {
+    id: "donaciones-2025",
+    titleKey: "ethicsTransparency.pdf.docs.donaciones2025.title",
+    category: "donaciones",
+    categoryLabelKey: "ethicsTransparency.pdf.tabs.donaciones",
+    year: "2025",
+    file: "https://fundacionantivirus-my.sharepoint.com/:b:/g/personal/desarrolladora_fundacionantivirusparaladesercion_org/IQCVhr1ZMrtoQ5lgcgF1VXojAcC3X6GJbPeHDEGWWSDS25Q?e=JGCMvG&download=1",
+  },
+  {
+    id: "reporte-2025",
+    titleKey: "ethicsTransparency.pdf.docs.reporte2025.title",
+    category: "reporte",
+    categoryLabelKey: "ethicsTransparency.pdf.tabs.reporte",
+    year: "2025",
+    file: "https://fundacionantivirus-my.sharepoint.com/:b:/g/personal/desarrolladora_fundacionantivirusparaladesercion_org/IQBfJ1GYpPyXSalKL5k1S-4vARKLI_pIV0L8O5R61w4NHU4?e=KS4nJE&download=1",
+  },
+  {
+    id: "ptee-2026",
+    titleKey: "ethicsTransparency.pdf.docs.ptee2026.title",
+    category: "ptee",
+    categoryLabelKey: "ethicsTransparency.pdf.tabs.ptee",
+    year: "2026",
+    file: "https://fundacionantivirus-my.sharepoint.com/:b:/g/personal/desarrolladora_fundacionantivirusparaladesercion_org/IQCDwooLG3VGQqyKfKnoF3KGAWP5W9pYdGKeu2gPodNvDJo?e=9gkhXW",
+  },
+  
+];
--- client/src/views/Foundation/Foundation.jsx
+++ client/src/views/Foundation/Foundation.jsx
@@ -1,13 +1,15 @@
 import FoundationATV from "./FoundationAtv/FoundationATV";
 import OrganizationalStructure from "./OrganizationalStructure/OrganizationalStructure";
 import EthicsTransparencyCard from "../../components/cards/EthicsTransparencyCard";
+import EthicsTransparency from "./EthicsTransparency/EthicsTransparency";
 
 function Foundation() {
   return (
     <div className="Foundation-container lg:pt-[145px]">
       <FoundationATV />
       <OrganizationalStructure />
-      <EthicsTransparencyCard />
+      {/* <EthicsTransparencyCard /> */}
+      <EthicsTransparency />
     </div>
   );
 }
