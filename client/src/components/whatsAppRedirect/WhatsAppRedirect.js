3c84c7be16fc0874fa2246af3e366f45c98bda8f
LinaMarinj <linamarin2011@gmail.com>
2026-06-17 08:07:08 -0500

    Feature: update WhatsApp message localization and redirect functionality ProVocacion

--- client/src/components/whatsAppRedirect/WhatsAppRedirect.js
+++ client/src/components/whatsAppRedirect/WhatsAppRedirect.js
@@ -1,13 +1,15 @@
+import { useTranslation } from "react-i18next";
+
 /**
  *
  * Función que genera una URL de redirección a WhatsApp con un mensaje predefinido y un número de teléfono específico.
  * @param {string} message Mensaje que se desea enviar a través de WhatsApp. Si no se proporciona, se utiliza un mensaje predeterminado.
  * @param {string} whatsAppNumber Número de WhatsApp al que se desea enviar el mensaje. Si no se proporciona, se utiliza un número predeterminado.
- * @param {function} t Función de traducción para obtener mensajes localizados.
  * @returns {string} URL de redirección a WhatsApp con el mensaje y número proporcionados
  * Si no se proporcionan, se utilizan valores predeterminados.
  */
-const WhatsAppRedirect = (message, whatsAppNumber, t) => {
+const WhatsAppRedirect = (message, whatsAppNumber) => {
+  const { t } = useTranslation();
 
   const defaultMessage = t("whatsappMessage.default");
   const finalMessage = message ? message : defaultMessage;
